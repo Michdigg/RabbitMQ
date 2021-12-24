e2968b21dfb464f53ef691beef5c548fcf48c209
michdigg <mich.digiorgio@gmail.com>
2021-12-24 12:19:27 +0100

    Second scenario and rules implemented

---
+++ Consumer/SecondScenario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Consumer
+{
+    public class SecondScenario
+    {
+        public static bool checkSecondScenario()
+        {
+            return SecondScenarioRule.firtCheckRule() || SecondScenarioRule.secondCheckRule();
+        }
+    }
+}
